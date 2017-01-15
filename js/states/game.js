d9a8fde078240c594b961ad6e31dda207921c445
Tobias Hvass Mølbak <tobiashmhtx@gmail.com>
2017-01-15 21:18:40 +0100

    Uploaded example game

--- js/states/game.js
+++ js/states/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 500, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
--- js/states/load.js
+++ js/states/load.js
@@ -4,8 +4,13 @@ var loadState = {
 		var loadingLabel = game.add.text(80, 150, 'loading...',
 			{font: '30px Courier', fill: '#ffffff'});
 
-		game.load.image('player', 'assets/dude.png');
-		game.load.image('win', 'assets/star.png');
+		game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+	    game.load.image('tiles-1', 'assets/tiles-1.png');
+	    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+	    game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
+	    game.load.image('starSmall', 'assets/star.png');
+	    game.load.image('starBig', 'assets/star2.png');
+	    game.load.image('background', 'assets/background2.png');
 	},
 
 	create: function() {
